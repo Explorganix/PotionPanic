549f8cb09c752c8a98e1bc846f5a6316503be8cc
LoveTrev <tlove@spsu.edu>
2016-08-29 19:54:35 -0400

    First Commit
    Splash screen implemented.

---
+++ Potion Panic!/Assets/ScalableCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalableCamera : MonoBehaviour {
+    public int pixelsPerUnit = 100;
+
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Camera>().orthographicSize = (float)Screen.height / 2 / pixelsPerUnit;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
