285281fc23c5012f07d370528716a3d9a22810fd
LoveTrev <tlove@spsu.edu>
2016-09-01 15:45:13 -0400

    Menu art
    Added menu art.

---
+++ Potion Panic!/Assets/Scripts/ScalableButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalableButton : MonoBehaviour {
+
+    public float targetScreenHeight = 2560;
+    public float targetScreenWidth = 1620;
+    public float yRatio;
+    public float xRatio;
+    public float aspectDifference;
+    public int pixelsPerUnit = 100;
+    public int screenHeight;
+    public int screenWidth;
+    public RectTransform tran;
+
+    // Use this for initialization
+    void Start () {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        yRatio = screenHeight / targetScreenHeight;
+        xRatio = screenWidth / targetScreenWidth;
+        tran = GetComponent<RectTransform>();
+
+        tran.position = new Vector3(tran.position.x * xRatio, tran.position.y * yRatio, 0);
+        tran.localScale = new Vector3(tran.localScale.x * xRatio, tran.localScale.y * yRatio, 0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ Potion Panic!/Assets/Scripts/ScalableCamera.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScalableCamera : MonoBehaviour {
+
+    public float targetScreenHeight = 2560;
+    public float targetScreenWidth = 1620;
+    public float targetAspectRatio;
+    public float deviceAspectRatio;
+    public float aspectDifference;
+    public int pixelsPerUnit = 100;
+    public int screenHeight;
+    public int screenWidth;
+    
+
+    // Use this for initialization
+    void Start () {
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+        targetAspectRatio = targetScreenWidth / targetScreenHeight;
+        deviceAspectRatio = (float)Screen.width / Screen.height;
+        aspectDifference = targetAspectRatio / deviceAspectRatio;
+        if (deviceAspectRatio <= targetAspectRatio)
+        {
+            GetComponent<Camera>().orthographicSize = targetScreenHeight / 2 / pixelsPerUnit;
+        }
+        else
+        {
+            GetComponent<Camera>().orthographicSize = targetScreenHeight / 2 / pixelsPerUnit * aspectDifference ;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
