42cfbd7fdab48db47cfedda5ab94923d85b3f769
LoveTrev <tlove@spsu.edu>
2016-09-05 16:13:52 -0400

    Cleaned up potions scripts
    Oraganizes and cleaned up potions script.

--- Potion Panic!/Assets/Potion.cs
+++ Potion Panic!/Assets/Potion.cs
@@ -3,48 +3,69 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 
-public class Potion : MonoBehaviour {
-
-    public Vector3 movementVector;
-    public Vector3 leftVatPos;
-    public Vector3 midVatPos;
-    public Vector3 rightVatPos;
-    public List<Vector3> vatVectors;
-    public int targetVectorIndex;
-    public int lastVectorIndex;
-    public int numRotations;
-    public bool movingLeft;
-    public bool rotating;
-    public Vector3 rotationDirection;
-
-    public bool fallingStraight = true;
-    public bool bouncing = false;
-    public float bounceHeight = 8f;
-    public float bounceTime;
-
+public class Potion : MonoBehaviour
+{
+    //Self References
     Rigidbody2D body;
 
+    //Adjustable Parameters
+    public float bounceHeight;
+    public float bounceTime;
+    public float fallSpeed;
+    public int numRotations;
+
+    //External Object References
+    public Vector3 movementVector;
+    public Vector3 leftTarget;
+    public Vector3 midTarget;
+    public Vector3 rightTarget;
+    public List<Vector3> targetVectors;
+
+    //Control Variables
+    protected Vector3 rotationDirection;
+    protected bool movingLeft;
+    protected bool rotating;
+    protected bool falling;
+    protected bool bouncing;
+    protected int targetVectorIndex;
+    protected int lastTargetVectorIndex;
+
+    void Awake()
+    {
+        falling = true;
+        bouncing = false;
+        bounceHeight = 14f;
+        bounceTime = 4f;
+        movingLeft = false;
+        rotationDirection = (movingLeft) ? Vector3.forward : Vector3.back;
+        body = GetComponent<Rigidbody2D>();
+
+    }
     // Use this for initialization
     void Start()
     {
-        vatVectors.Add(leftVatPos);
-        vatVectors.Add(midVatPos);
-        vatVectors.Add(rightVatPos);
-        body = GetComponent<Rigidbody2D>();
-        movingLeft = false;
-        rotationDirection = (movingLeft) ? Vector3.forward : Vector3.back;
+        targetVectors.Add(leftTarget);
+        targetVectors.Add(midTarget);
+        targetVectors.Add(rightTarget);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (fallingStraight)
+        if (falling)
         {
             body.MovePosition(transform.position + movementVector * Time.deltaTime);
         }
-
     }
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Trampoline")
+        {
+            UpdateRotation();
+            UpdateBounce();
+        }
+    }
 
     IEnumerator Rotate()
     {
@@ -55,52 +76,23 @@ public class Potion : MonoBehaviour {
             transform.Rotate(rotationDirection, 360 * numRotations * Time.deltaTime / bounceTime);
             yield return null;
         }
-
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    private void UpdateRotation()
     {
-        if (other.gameObject.tag == "Trampoline")
+        if (!rotating)
         {
-            if (!rotating)
-            {
-                StartCoroutine("Rotate");
-            }
-            fallingStraight = false;
-            bouncing = false;
-            StopCoroutine("Bounce");
-            lastVectorIndex = targetVectorIndex;
-            if(lastVectorIndex <= 0)
-            {
-                movingLeft = false;
-            }
-            else if(lastVectorIndex >= 2)
-            {
-                movingLeft = true;
-            }
-
-            targetVectorIndex += (movingLeft) ?  -1 : 1;
-            rotationDirection = (movingLeft) ? Vector3.forward : Vector3.back;
-            //if (movingLeft)
-            //{              
-            //    targetVectorIndex--;
-            //}
-            //else
-            //{
-            //    targetVectorIndex++;
-            //}
-            StartCoroutine("Bounce");
-
+            StartCoroutine("Rotate");
         }
     }
 
-
     IEnumerator Bounce()
     {
-        Vector3 dest = vatVectors[targetVectorIndex];
+        Vector3 dest = targetVectors[targetVectorIndex];
         float time = bounceTime;
+
         if (bouncing) yield break;
-        
+
         bouncing = true;
         Vector3 startPos = transform.position;
         float timer = 0.0f;
@@ -109,11 +101,28 @@ public class Potion : MonoBehaviour {
         {
             float height = Mathf.Sin(Mathf.PI * timer) * bounceHeight;
             body.MovePosition(Vector3.Lerp(startPos, dest, timer) + Vector3.up * height);
-            //transform.Rotate(Vector3.back, 720 * (Time.deltaTime / bounceTime));
-
             timer += Time.deltaTime / time;
             yield return null;
         }
         bouncing = false;
+    }
+
+    private void UpdateBounce()
+    {
+        falling = false;
+        StopCoroutine("Bounce");
+        bouncing = false;
+        lastTargetVectorIndex = targetVectorIndex;
+        if (lastTargetVectorIndex <= 0)
+        {
+            movingLeft = false;
+        }
+        else if (lastTargetVectorIndex >= 2)
+        {
+            movingLeft = true;
+        }
+        targetVectorIndex += (movingLeft) ? -1 : 1;
+        rotationDirection = (movingLeft) ? Vector3.forward : Vector3.back;
+        StartCoroutine("Bounce");
     }
 }
