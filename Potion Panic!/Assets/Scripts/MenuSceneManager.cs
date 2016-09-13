fee38dba2039f1a4ef25f550fcd21c9a603b0794
LoveTrev <tlove@spsu.edu>
2016-09-12 20:34:59 -0400

    MenuSceneManager
    Menu Manager working

---
+++ Potion Panic!/Assets/Scripts/MenuSceneManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour {
+
+    SceneManager sm;
+	// Use this for initialization
+	void Start () {
+        sm = new SceneManager();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void LoadLevel(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
